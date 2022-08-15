cbb2f7b693d8ea7b15afbcfd62468718a25f6dcb
Frazer Findlater <frazer.findlater@gmail.com>
2022-08-15 19:32:35 +0100

    Page build

--- src/assets/js/test.js
+++ src/assets/js/test.js
@@ -1,9 +1,55 @@
-console.log("Testing JS import file one.");
-// Test JavaScript, to ensure Babel is working.
+const swiper = new Swiper('.swiper', {
+    effect: "fade",
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: true,
+    speed: 2000,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
 
-const array1 = ['a', 'b', 'c'];
+/* ------------------------------------ */
+/* Scroll To Functionality */
+/* ------------------------------------ */
 
-array1.forEach(element => console.log(element));
-
-const matches = document.querySelectorAll("div");
-console.log(matches);
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
